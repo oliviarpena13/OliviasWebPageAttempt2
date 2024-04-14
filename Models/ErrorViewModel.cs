36e90d564ab4546b11956b32ff376392558a7be9
oliviarpena13 <olivia.r.pena13@gmail.com>
2024-04-14 12:52:40 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OliviasWebPageAttempt2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
